81c05d68280d1284c0d6e491b7a8a4acc0917aa7
Akshita Swarnkar <88489579+Shanvie@users.noreply.github.com>
2022-06-23 04:24:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/nnlJ1QGZS/modelLoaded.jason',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/nnlJ1QGZS/modelLoaded.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
